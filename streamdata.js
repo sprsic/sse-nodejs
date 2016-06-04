e82ad61d8f5919ecec2ea5755530fb3e4ce6e47b
Hydra <s.prsic@gmail.com>
2016-06-04 22:33:28 +0200

    Tidy

--- streamdata.js
+++ streamdata.js
@@ -1,4 +1,3 @@
-document.getElementById('data').innerHTML = "Some useless data";
 var source = new EventSource("/event");
 	source.onmessage = function(event) {
 	document.getElementById("data").innerHTML += event.data + "<br>";
